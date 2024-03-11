1a75ab3fb4566d9ff96846373a2d8ffd16e34a79
Fikri <fikfan@gmail.com>
2024-03-11 15:16:30 +0800

    edited footer/header, added generator, fixed main spacing, added copy function

--- hadis/src/components/CardComponent.vue
+++ hadis/src/components/CardComponent.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <div class="bg-slate-500 rounded-xl p-4 shadow-xl text-center grid gap-4">
-      <p class="text-lg font-semibold">{{ this.hadis.title }}</p>
-      <p class="p-2 bg-red-200 italic font-light text-center rounded indent-6">{{ this.hadis.hadeeth }}</p>
-      <div>
-        <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-white bg-black rounded-full">{{ this.hadis.attribution }}</span>
-        <span class="inline-flex items-center justify-center px-2 py-1 mr-2 text-xs font-bold leading-none text-blue-600 bg-green-300 rounded-full">{{ this.hadis.grade }}</span>
+    <div v-if="hadis" class="bg-black rounded-xl p-4 shadow-xl text-center grid gap-4" :class="hadis ? 'mb-4' : ''">
+      <p class="text-xs sm:text-base p-2 text-white border-white border italic text-justify rounded indent-6">{{ hadis.hadeeth }}</p>
+      <div class="flex gap-2 justify-center">
+        <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-black bg-white rounded-full">{{ hadis.attribution }}</span>
+        <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none bg-green text-white border border-green rounded-full">{{ hadis.grade.slice(0, -1) }}</span>
       </div>
-      <p class="font-bold">Explanation</p>
-      <p class="p-2 bg-red-300 rounded indent-6 text-justify">{{ this.hadis.explanation }}</p>
-      <p class="font-bold font-serif text-2xl">بارك الله فيك</p>
+      <p class="font-bold text-sm md:text-base text-white">Explanation</p>
+      <p class="p-2 bg-black text-white border border-white rounded indent-6 text-justify text-xs sm:text-base">{{ hadis.explanation }}</p>
     </div>
-    <button @click="fetchHadeeth()" class="bg-white text-black p-4">Fetch Hadis</button>
-    <p>{{ num }}</p>
+    <button @click="fetchHadeeth()" id="fetch" class="bg-white text-black p-3 sm:p-4 text-sm sm:text-base rounded-xl hover:bg-black hover:text-white w-fit mx-auto flex">
+      {{ hadis ? 'Another one?' : (loading ? 'Loading...' : 'Generate Hadeeth') }}
+    </button>
+    <button v-if="hadis" @click="changeText" class="bg-white text-black p-3 sm:p-4 mt-2 text-sm sm:text-base rounded-xl hover:bg-black hover:text-white w-fit mx-auto flex">{{ buttonText }}</button>
   </div>
-  
 </template>
 
 <script>
@@ -23,20 +22,37 @@ export default {
     return {
       num: null,
       hadis: null,
+      loading: false,
+      buttonText: 'Copy?',
     }
   },
   methods: {
     async fetchHadeeth() {
+      if (this.loading) return; // Prevent multiple requests
+      
+      this.loading = true;
       this.num = Math.floor(Math.random() * (3200 - 2900 + 1)) + 2900;
       const response = await fetch(`https://hadeethenc.com/api/v1/hadeeths/one/?language=en&id=${this.num}`);
       this.hadis = await response.json();
+
+      setTimeout(() => {
+        this.loading = false; // Allow another request after timeout
+      }, 2000);
+    },
+    changeText() {
+      this.copy();
+      this.buttonText = 'Copied! Thank you for your support! ❤️';
+      setTimeout(() => {
+        this.buttonText = 'Copy?';
+      }, 2000);
+    },
+    copy() {
+      const text = `
+        ${this.hadis.hadeeth}\nGrade: ${this.hadis.grade.slice(0, -1)} - ${this.hadis.attribution}\n\nExplanation:\n\n${this.hadis.explanation}}
+      `
+      navigator.clipboard.writeText(text);
     }
   },
-  async mounted() {
-    this.num = Math.floor(Math.random() * (3200 - 2900 + 1)) + 2900;
-    const response = await fetch(`https://hadeethenc.com/api/v1/hadeeths/one/?language=en&id=2962`);
-    this.hadis = await response.json();
-  }
 }
 </script>
 
